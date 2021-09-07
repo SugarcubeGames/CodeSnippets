5bbd4fcf4a77105fef512ae505a19646753619e5
SugarcubeGames <74308584+SugarcubeGames@users.noreply.github.com>
2021-09-07 11:33:18 -0800

    Pocket Worlds
    3D map generation - Uses C# implementation of LibNoise library.

---
+++ Pocket World (Cube Terrain generation Test)/Biomes/Plains.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PWorlds.Gen.Biomes{
+	public class Plains:Biome{
+
+		public override void init()
+		{
+			this.sampleSizeX = 4.0f;
+			this.sampleSizeY = 4.0f;
+
+			this.sampleOffsetX = 2.0f;
+			this.sampleOffsetY = 1.0f;
+
+			this.numModules = 1;
+
+			this.numOctaves = 6;
+			this.frequency = .6f;
+			this.persistence = .1f;
+
+			//this.numOctaves = 6;
+			//this.frequency = 2f;
+			//this.persistence = .5f;
+
+			/*
+			this.topColor = Color.green;
+			this.bottomColor = new Color(0.0f, 0.0f, 0.0f);
+			*/
+
+			//Desert coloration is easier to look at, will have to adjust plains
+			//this.topColor = new Color(1.0f,(195.0f/255.0f), 0.0f);
+			//this.bottomColor = Color.red;
+			this.topColor = Color.green;
+			this.bottomColor = Color.black;
+			this.numDivisions = 16;
+
+			this.heightMultiplier = 6.0f;
+
+			//Water variables
+			this.hasWater = true;
+			this.waterLevel = 4;
+			//transparent cyan color
+			this.waterColor = new Color(0.0f, 1.0f, 1.0f, .25f);
+
+		}
+	}
+}
---
+++ Pocket World (Cube Terrain generation Test)/WorldAnchor.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using LibNoise;
+using LibNoise.Generator;
+
+using PWorlds.Gen.Biomes;
+/*
+ * The world anchor is the script which holds all the world data for the
+ * curently-active world.  For now, it will also call the generation script.
+ * */
+namespace PWorlds.Gen
+{
+	public class WorldAnchor : MonoBehaviour {
+
+		public MapSize mapSize;
+
+		// Use this for initialization
+		void Start () {
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			if(Input.GetKeyDown(KeyCode.G)){
+				Biome plains = new Plains();
+				plains.init();
+				this.gameObject.GetComponent<MeshRenderer>().material.mainTexture 
+					= Generate(plains, MapSize.LARGE);
+			}
+		}
+
+		//Rceive a biome that will dictate map generation
+		private static Texture2D Generate(Biome biome, int size){
+
+			Texture2D texture;
+
+			Perlin myPerlin = new Perlin();
+			myPerlin.Seed = (int)(Random.Range(-2.0f, 2.0f)*10000f);
+			myPerlin.OctaveCount = biome.numOctaves;
+			myPerlin.Frequency = biome.frequency;
+			myPerlin.Persistence = biome.persistence;
+
+			ModuleBase myModule = myPerlin;
+			
+			Noise2D heightmap;
+			heightmap = new Noise2D(size, size, myModule);
+			
+			heightmap.GeneratePlanar(
+				biome.sampleOffsetX,
+				biome.sampleOffsetX + biome.sampleSizeX,
+				biome.sampleOffsetY,
+				biome.sampleOffsetY + biome.sampleSizeY);
+
+			texture = heightmap.GetTexture(GradientPresets.Terrain);
+
+			WorldMap map = new WorldMap(heightmap, biome.numDivisions, size,
+			                            biome);
+
+			//cubeRenderer.material.mainTexture = texture;
+			return texture;
+		}
+	}
+}
